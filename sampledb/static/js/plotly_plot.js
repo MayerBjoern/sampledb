63c8f29de1f318b8aff53f0debb0a3b8470a646e
Rene Noffke <ren.noffke@alumni.fh-aachen.de>
2021-02-18 10:02:58 +0100

    added color

--- sampledb/static/js/plotly_plot.js
+++ sampledb/static/js/plotly_plot.js
@@ -27,10 +27,12 @@ function plotlyPlot(json_string) {
 function plot(json_string) {
     var json_string_key_list = Object.keys(json_string);
     Plotly.newPlot('plotly_plot_div');
+    var layout = {layout: {barmode: 'stack'}};
+    if(json_string_key_list.includes("layout")) {
+        layout_string = json_string["layout"];
+    }
     for(var i in json_string_key_list) {
         var json_data = json_string[json_string_key_list[i]]
-        var layout = json_data['layout']
-        console.log(layout)
-        Plotly.plot('plotly_plot_div', {data: [json_string[json_string_key_list[i]]], layout: {barmode: 'stack'  }});
+        Plotly.plot('plotly_plot_div', {data: [json_string[json_string_key_list[i]]], 'layout': { layout } });
     }
 }
